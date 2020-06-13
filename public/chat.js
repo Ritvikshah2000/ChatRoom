b9dcdd3e56531c27adb02de4c03ced4211c4f153
jovan <jovanbapla@gmail.com>
2020-06-12 17:18:57 -0700

    rough outline

---
+++ public/chat.js
@@ -0,0 +1,43 @@
+
+//make connection
+var socket= io.connect("http://localhost:4000");
+
+var text= document.getElementById("text");
+var btn= document.getElementById("button");
+var message= document.getElementById("message");
+var user = document.getElementById("name");
+
+
+btn.addEventListener("click", function(){
+    //send socket data to server
+    socket.emit("chat", {
+        message: message.value,
+        name: user.value
+    });
+   
+    message.value="";
+});
+
+message.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     socket.emit("chat", {
+        message: message.value,
+        name: user.value
+    })
+   
+    message.value="";
+     }
+  });
+
+//listen for data coming from server
+socket.on("chat", function(data){
+    var node= document.createElement("h3");
+    var chatResponse= document.createTextNode(data.name+": "+ data.message);
+    node.appendChild(chatResponse);
+    document.getElementById("chat").appendChild(node);
+    var box = document.getElementById('chat');
+    box.scrollTop = box.scrollHeight;
+
+})
+
